cfad932285ab74b01e929ee8ef80817e58229339
Daniel Radu <89708521+dvmizew@users.noreply.github.com>
2024-11-05 18:35:49 +0200

    added more of those voice lines

--- tema2/lib/dictionary/dictionary.h
+++ tema2/lib/dictionary/dictionary.h
@@ -2,7 +2,7 @@
 #include <avr/pgmspace.h>
 
 // furat de la regele https://github.com/oreoezi si adaugat cateva replici
-const char str0[] PROGMEM = "piticule cine esti ma"; 
+const char str0[] PROGMEM = "piticule cine esti ma?"; 
 const char str1[] PROGMEM = "zero munca"; 
 const char str2[] PROGMEM = "alta gasca in alta traista"; 
 const char str3[] PROGMEM = "switch switch router";  
@@ -54,16 +54,55 @@ const char str48[] PROGMEM = "defolt ghetoi";
 const char str49[] PROGMEM = "nu sariti peste fire";
 const char str50[] PROGMEM = "toate cele bune copii";
 const char str51[] PROGMEM = "si voua copii";
-const char str52[] PROGMEM = "ati folosit gipitiul";
-const char str53[] PROGMEM = "tu stii ce inseamna USO";
-const char str54[] PROGMEM = "ce o fi ala folder";
-const char str55[] PROGMEM = "ce o fi aia partitie";
-const char str56[] PROGMEM = "ti-am zis eu sa intri acolo";
+const char str52[] PROGMEM = "ati folosit gipitiul?";
+const char str53[] PROGMEM = "tu stii ce inseamna USO?";
+const char str54[] PROGMEM = "ce o fi ala folder?";
+const char str55[] PROGMEM = "ce o fi aia partitie?";
+const char str56[] PROGMEM = "ti-am zis eu sa intri acolo?";
 const char str57[] PROGMEM = "te joci acasa";
 const char str58[] PROGMEM = "am spus castraveti";
 const char str59[] PROGMEM = "rong copilu";
-const char str60[] PROGMEM = "ce cablu foloseste conexiunea wireless";
+const char str60[] PROGMEM = "ce cablu foloseste conexiunea wireless?";
 const char str61[] PROGMEM = "nu sariti peste fire";
+const char str62[] PROGMEM = "ursuletul din erasmus";
+const char str63[] PROGMEM = "crestem in valoare";
+const char str64[] PROGMEM = "nu vreau sa vad elefanti";
+const char str65[] PROGMEM = "sub nivelul marii";
+const char str66[] PROGMEM = "nu ai vlanul null";
+const char str67[] PROGMEM = "tie oricum nu iti iese nimic";
+const char str68[] PROGMEM = "tu lucrezi ca sa lucrezi";
+const char str69[] PROGMEM = "noi lucram corect pe etape";
+const char str70[] PROGMEM = "perpetuezi greseala";
+const char str71[] PROGMEM = "perspectiva";
+const char str72[] PROGMEM = "in momentul de fata";
+const char str73[] PROGMEM = "munca ta e zero";
+const char str74[] PROGMEM = "nu va faceti programatori";
+const char str75[] PROGMEM = "folositi doar packet tracer";
+const char str76[] PROGMEM = "iti lipsesc elemente";
+const char str77[] PROGMEM = "te-ai documentat?";
+const char str78[] PROGMEM = "cablarea ta e zero";
+const char str79[] PROGMEM = "isepistu";
+const char str80[] PROGMEM = "la fel si voua copii";
+const char str81[] PROGMEM = "spor la treaba, o zi buna";
+const char str82[] PROGMEM = "nu ne intelegem";
+const char str83[] PROGMEM = "nu prea ai fost pe aici";
+const char str84[] PROGMEM = "nu pierdem din vedere primul semestru";
+const char str85[] PROGMEM = "sintaxa";
+const char str86[] PROGMEM = "scrie bine colega ta?";
+const char str87[] PROGMEM = "vreau sa am pasul 2";
+const char str88[] PROGMEM = "nu ai cu ce sa lucrezi";
+const char str89[] PROGMEM = "asa e in tenis";
+const char str90[] PROGMEM = "te las sa intri, dar fara prezenta";
+const char str91[] PROGMEM = "scopul si durata";
+const char str92[] PROGMEM = "cu ce ocazie pe la noi?";
+const char str93[] PROGMEM = "ai pierdut trenul";
+const char str94[] PROGMEM = "tot ce facem noi are sens";
+const char str95[] PROGMEM = "cred ca ne mai trebuie un doctorat";
+const char str96[] PROGMEM = "cati dintre voi aveti permis de conducere?";
+const char str97[] PROGMEM = "axul drumului se mai numeste si refugiu";
+const char str98[] PROGMEM = "pauza noastra ca de obicei este la fara 20";
+const char str99[] PROGMEM = "nu iti depanez eu tie echipamentul";
+const char str100[] PROGMEM = "partea asta uit-o copilu";
 
 const char *const dictionary[] PROGMEM = {
     str0, str1, str2, str3, str4, str5, str6, str7,
@@ -73,7 +112,12 @@ const char *const dictionary[] PROGMEM = {
     str32, str33, str34, str35, str36, str37, str38, str39,
     str40, str41, str42, str43, str44, str45, str46, str47,
     str48, str49, str50, str51, str52, str53, str54, str55,
-    str56, str57, str58, str59, str60, str61
+    str56, str57, str58, str59, str60, str61, str62, str63,
+    str64, str65, str66, str67, str68, str69, str70, str71,
+    str72, str73, str74, str75, str76, str77, str78, str79,
+    str80, str81, str82, str83, str84, str85, str86, str87,
+    str88, str89, str90, str91, str92, str93, str94, str95,
+    str96, str97, str98, str99, str100
 };
 
 const uint8_t dictionaryLen = sizeof(dictionary) / sizeof(dictionary[0]);
