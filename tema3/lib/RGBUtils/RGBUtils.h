83c0c1d5b91a0cbe289387753b60b4efae7fc62c
Daniel Radu <89708521+dvmizew@users.noreply.github.com>
2024-11-18 23:55:21 +0200

    moved some code over

--- tema3/lib/RGBUtils/RGBUtils.h
+++
@@ -1,9 +0,0 @@
-#ifndef RGBUTILS_H
-#define RGBUTILS_H
-
-#include <Arduino.h>
-
-void setRGBColor(int redPin, int greenPin, int bluePin, int color);
-void turnOffRGB(int redPin, int greenPin, int bluePin);
-
-#endif
