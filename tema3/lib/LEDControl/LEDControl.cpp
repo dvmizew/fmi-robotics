5783cbd6527359c59cee6d04f3a3b2705ae80634
Daniel Radu <89708521+dvmizew@users.noreply.github.com>
2024-11-18 23:53:41 +0200

    fixed the led things and moved the RGBUtils functions over there

--- tema3/lib/LEDControl/LEDControl.cpp
+++ tema3/lib/LEDControl/LEDControl.cpp
@@ -1,25 +1,33 @@
 #include "LEDControl.h"
 
-// Player 1 RGB LED pins
-#define RGB_LED_PLAYER1_RED 9
-#define RGB_LED_PLAYER1_GREEN 10
-#define RGB_LED_PLAYER1_BLUE 11
-
-// Player 2 RGB LED pins
-#define RGB_LED_PLAYER2_RED 6
-#define RGB_LED_PLAYER2_GREEN 5
-#define RGB_LED_PLAYER2_BLUE 3
-
-void initializeLEDs() {
+void initializeRGBLEDs() {
     // Player 1 RGB LED pins
-    pinMode(RGB_LED_PLAYER1_RED, OUTPUT);
-    pinMode(RGB_LED_PLAYER1_GREEN, OUTPUT);
-    pinMode(RGB_LED_PLAYER1_BLUE, OUTPUT);
+    pinMode(PLAYER1_RED, OUTPUT);
+    pinMode(PLAYER1_GREEN, OUTPUT);
+    pinMode(PLAYER1_BLUE, OUTPUT);
 
     // Player 2 RGB LED pins
-    pinMode(RGB_LED_PLAYER2_RED, OUTPUT);
-    pinMode(RGB_LED_PLAYER2_GREEN, OUTPUT);
-    pinMode(RGB_LED_PLAYER2_BLUE, OUTPUT);
+    pinMode(PLAYER2_RED, OUTPUT);
+    pinMode(PLAYER2_GREEN, OUTPUT);
+    pinMode(PLAYER2_BLUE, OUTPUT);
+}
+
+void initializeButtonLEDs() {
+    // initialize the button LEDs
+    pinMode(LED1_PIN, OUTPUT);
+    pinMode(LED2_PIN, OUTPUT);
+    pinMode(LED3_PIN, OUTPUT);
+    pinMode(LED4_PIN, OUTPUT);
+    pinMode(LED5_PIN, OUTPUT);
+    pinMode(LED6_PIN, OUTPUT);
+
+    // turn on all button LEDs
+    digitalWrite(LED1_PIN, HIGH);
+    digitalWrite(LED2_PIN, HIGH);
+    digitalWrite(LED3_PIN, HIGH);
+    digitalWrite(LED4_PIN, HIGH);
+    digitalWrite(LED5_PIN, HIGH);
+    digitalWrite(LED6_PIN, HIGH);
 }
 
 void blinkLED(int pin) {
@@ -31,16 +39,28 @@ void blinkLED(int pin) {
     }
 }
 
-void setPlayerRGBColor(int player, int color) {
-    if (player == 1)
-        setRGBColor(RGB_LED_PLAYER1_RED, RGB_LED_PLAYER1_GREEN, RGB_LED_PLAYER1_BLUE, color);
-    else if (player == 2)
-        setRGBColor(RGB_LED_PLAYER2_RED, RGB_LED_PLAYER2_GREEN, RGB_LED_PLAYER2_BLUE, color);
+void setRGBColor(int redPin, int greenPin, int bluePin, int color) {
+    digitalWrite(redPin, color == 1); // 1 - RED
+    digitalWrite(greenPin, color == 2); // 2 - GREEN
+    digitalWrite(bluePin, color == 3); // 3 - BLUE
 }
 
-void turnOffRGB(int player) {
+void setPlayerRGBColor(int player, int color) {
     if (player == 1)
-        turnOffRGB(RGB_LED_PLAYER1_RED, RGB_LED_PLAYER1_GREEN, RGB_LED_PLAYER1_BLUE);
+        setRGBColor(PLAYER1_RED, PLAYER1_GREEN, PLAYER1_BLUE, color);
     else if (player == 2)
-        turnOffRGB(RGB_LED_PLAYER2_RED, RGB_LED_PLAYER2_GREEN, RGB_LED_PLAYER2_BLUE);
+        setRGBColor(PLAYER2_RED, PLAYER2_GREEN, PLAYER2_BLUE, color);
+}
+
+void turnOffRGB(int redPin, int greenPin, int bluePin) {
+    digitalWrite(redPin, LOW);
+    digitalWrite(greenPin, LOW);
+    digitalWrite(bluePin, LOW);
+}
+
+void turnOffPlayerRGB(int player) {
+    if (player == 1)
+        turnOffRGB(PLAYER1_RED, PLAYER1_GREEN, PLAYER1_BLUE);
+    else if (player == 2)
+        turnOffRGB(PLAYER2_RED, PLAYER2_GREEN, PLAYER2_BLUE);
 }
--- tema3/lib/LEDControl/LEDControl.h
+++ tema3/lib/LEDControl/LEDControl.h
@@ -2,11 +2,31 @@
 #define LEDCONTROL_H
 
 #include <Arduino.h>
-#include "RGBUtils.h"
 
-void initializeLEDs();
+// Button LED pin
+#define LED1_PIN A1
+#define LED2_PIN A2
+#define LED3_PIN A3
+#define LED4_PIN A4
+#define LED5_PIN A5
+#define LED6_PIN 7
+
+// Player 1 RGB LED pins
+#define PLAYER1_RED 6
+#define PLAYER1_GREEN 4
+#define PLAYER1_BLUE 5
+
+// Player 2 RGB LED pins
+#define PLAYER2_RED 3
+#define PLAYER2_GREEN 1
+#define PLAYER2_BLUE 2
+
+void initializeRGBLEDs();
+void initializeButtonLEDs();
 void setPlayerRGBColor(int player, int color);
-void turnOffRGB(int player);
+void turnOffPlayerRGB(int player);
+void setRGBColor(int redPin, int greenPin, int bluePin, int color);
+void turnOffRGB(int redPin, int greenPin, int bluePin);
 void blinkLED(int pin);
 
 #endif
