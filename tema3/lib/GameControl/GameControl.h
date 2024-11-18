07c7a015f856a28aa7c70d96c743918957702cff
Daniel Radu <89708521+dvmizew@users.noreply.github.com>
2024-11-18 20:03:32 +0200

    SPI function added

--- tema3/lib/GameControl/GameControl.h
+++ tema3/lib/GameControl/GameControl.h
@@ -3,16 +3,19 @@
 
 #define BUZZER_PIN 4 // buzzer pin for playing sounds
 #define SS_PIN 10 // slave select pin for SPI communication
+#define SERVO_PIN 6 // servo pin for moving the button
 
 #include <Arduino.h>
 #include <Servo.h>
 #include <SPI.h>
 
+void initializeSerial();
 void initializeServo();
 void startGame();
 void updateGame();
 void playCorrectSound();
 void initializeSPI();
 void sendCommandToSlave(int command);
+void printDebug(String message);
 
 #endif
