ace5dd5cc7f54b60710fa51638914783ce460b51
Daniel Radu <89708521+dvmizew@users.noreply.github.com>
2024-11-18 20:00:51 +0200

    use macros for LCD pins

--- tema3/lib/Display/Display.h
+++ tema3/lib/Display/Display.h
@@ -3,6 +3,14 @@
 
 #include <LiquidCrystal.h>
 
+// LCD pins
+#define RS 12
+#define E 11
+#define D4 5
+#define D5 4
+#define D6 3
+#define D7 2
+
 void initializeLCD();
 void displayWelcomeMessage();
 void updateScore(int player1Score, int player2Score);
