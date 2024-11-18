18fb951f9f05a8a6ba4bae63e67712f4eb64c87f
Daniel Radu <89708521+dvmizew@users.noreply.github.com>
2024-11-18 20:09:29 +0200

    changed the way slave handle the master commands

--- tema3/lib/slave/slave.cpp
+++ tema3/lib/slave/slave.cpp
@@ -13,12 +13,26 @@ void setup() {
 void loop() {
     if (digitalRead(SS) == LOW) { // if the slave select pin is enabled
         int command = SPI.transfer(0); // read the command from the master (0 is a dummy byte)
-        handleCommand(command);
+        handleMasterCommand(command);
     }
 }
 
-void handleCommand(int command) {
-    digitalWrite(RGB_LED_RED, command == 1);
-    digitalWrite(RGB_LED_GREEN, command == 2);
-    digitalWrite(RGB_LED_BLUE, command == 3);
+void handleMasterCommand(int command) {
+    // handle the command received from the master
+    switch (command) {
+        case 1:
+            setRGBColor(RGB_LED_RED, RGB_LED_GREEN, RGB_LED_BLUE, 1); // set to RED
+            break;
+        case 2:
+            setRGBColor(RGB_LED_RED, RGB_LED_GREEN, RGB_LED_BLUE, 2); // set to GREEN
+            break;
+        case 3:
+            setRGBColor(RGB_LED_RED, RGB_LED_GREEN, RGB_LED_BLUE, 3); // set to BLUE
+            break;
+        case 4:
+            blinkLED(RGB_LED_RED); // blink the RED LED as an error signal
+            break;
+        default:
+            turnOffRGB(RGB_LED_RED, RGB_LED_GREEN, RGB_LED_BLUE);
+    }
 }
