311608a44e6a471ca0c5093b0d18664d8dfaa1f0
Daniel Radu <89708521+dvmizew@users.noreply.github.com>
2024-11-19 10:46:27 +0200

    added debouncing

--- tema3/lib/Buttons/Buttons.cpp
+++ tema3/lib/Buttons/Buttons.cpp
@@ -5,20 +5,41 @@
 #define BUTTON2_THRESHOLD 400
 #define BUTTON3_THRESHOLD 600
 
+#define DEBOUNCE_DELAY 50 
+#define NO_BUTTON_PRESSED -1 // default value for no button pressed
+
 void initializeButtons() {
     pinMode(BUTTON_PIN, INPUT_PULLUP);
 }
 
 int readMultiplexedButton() {
+    static int lastButton = NO_BUTTON_PRESSED; // last button pressed
+    static unsigned long lastDebounceTime = 0;
+
     int buttonValue = analogRead(BUTTON_PIN);
+    int detectedButton = NO_BUTTON_PRESSED;
 
     if (buttonValue < BUTTON1_THRESHOLD) {
-        return 1; // first button
-    } else if (buttonValue < BUTTON2_THRESHOLD) {
-        return 2; // second button
-    } else if (buttonValue < BUTTON3_THRESHOLD) {
-        return 3; // third button
+        detectedButton = 1; // button 1 is pressed
+    } 
+    else if (buttonValue < BUTTON2_THRESHOLD) {
+        detectedButton = 2; // button 2 is pressed
+    } 
+    else if (buttonValue < BUTTON3_THRESHOLD) {
+        detectedButton = 3; // button 3 is pressed
     }
 
-    return -1; // no button was pressed
+    if (detectedButton != lastButton) {
+        lastDebounceTime = millis(); // reset the debounce timer
+    }
+
+    if ((millis() - lastDebounceTime) > DEBOUNCE_DELAY) {
+        // if the button value has been stable for a while
+        if (detectedButton != lastButton) {
+            lastButton = detectedButton;
+            return detectedButton;
+        }
+    }
+
+    return NO_BUTTON_PRESSED;
 }
